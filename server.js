9c4cac2ae65d362d6e34afa5b7aa9586bafb2c09
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-10 16:45:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,73 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+  title : 'Article One | Nagarajan J',
+  heading : 'Artcle ONE',
+  date : 'Aug 7, 2017',
+  content :`                
+            <p>
+                This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017.
+            </p>
+            <p>
+                This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017.
+            </p>
+            <p>
+                This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017. This a created for Article One on 7th Aug 2017. This a created for ARTICLE ONE on 7th Aug 2017.
+            </p>
+            `
+};
+
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viexport" content="width+device+width, initial-scroll-1">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                
+                <div>
+                    ${content}    
+                </div>
+            </div>
+        </body>
+        
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+//   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
