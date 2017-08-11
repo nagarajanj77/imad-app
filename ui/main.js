72e4de108cdd26add5d881fb26b92475178b07f5
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-11 14:55:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,16 @@ element.innerHTML = "New value : Hai, Nagarajan J";
 
 //move the image
 var img = document.getElementById("madi");
+
+var marginLeft = 0;
+
+function moveRight() {
+ marginLeft = marginLeft + 10;
+ img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function () {
-  img.style.marginLeft = "100px";
+  var interval = setInterval(moveRight, 100);
+//  img.style.marginLeft = "100px";
 };
 
