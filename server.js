48f749b49f73928f50b6dc8a8a9b0a2f5647f0c6
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-14 15:13:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,13 +138,12 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) {
     //get name from request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
-    
-    
+    // json Java Script Object Notation
     res.send(JSON.stringify(names));
 });
 
