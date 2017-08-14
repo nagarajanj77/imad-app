06d2ac82282b9fde5882f1c1c681d1f4d001b381
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-14 12:41:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 
 //change text
@@ -9,17 +9,34 @@ console.log('Loaded!');
 
 
 //move the image
-var img = document.getElementById("madi");
+//var img = document.getElementById("madi");
 
-var marginLeft = 0;
+//var marginLeft = 0;
 
-function moveRight() {
- marginLeft = marginLeft + 1;
- img.style.marginLeft = marginLeft + 'px';
-}
+//function moveRight() {
+// marginLeft = marginLeft + 1;
+// img.style.marginLeft = marginLeft + 'px';
+//}
 
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
+//img.onclick = function () {
+//  var interval = setInterval(moveRight, 50);
 //  img.style.marginLeft = "100px";
-};
+//};
 
+
+// Counter code
+var button = document.getElementByID("counter");
+var counter = 0;
+
+button.onclick = function () {
+    
+    // make a request to end point
+    
+    // capture the response and store in a variable
+    
+    // render the variable in correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+    
+};
