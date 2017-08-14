238eaec1588a0d4e5d66a123cec187de19baba29
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-14 14:14:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,7 +47,7 @@ button.onclick = function () {
     };
     
     // make a request
-    request.open('GET', 'http://nagarajanj77.imad.hasura-app.io/', true);
+    request.open('GET', 'http://nagarajanj77.imad.hasura-app.io/counter', true);
     request.send(null);
     
 };
