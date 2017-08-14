ac2930077c09bf7ff5295aeff2884dc097324125
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-14 14:52:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -51,3 +51,25 @@ button.onclick = function () {
     request.send(null);
     
 };
+
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+  // make request to server
+  
+  // capture list of name and render it as list
+  var names = [name1, name2, name3, name4];
+  var list = '';
+  for (var i=0; i < names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.iinerHTML = list;
+  
+};
+
+
+
