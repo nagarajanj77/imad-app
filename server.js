38081951a7bef85698dfc25f7e352b9d0607ce22
nagarajanj77 <30690305+nagarajanj77@users.noreply.github.com>
2017-08-21 11:33:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/submit-name', function(req, res) {
 app.get('/articles/:articleName', function (req, res) {
 //   articleName = article-one
 //   articles(articleName) = {} content object for article one
-    pool.query("SELECT * FROM ARTICLE WHERE TITLE = '" + req.params.articleName + "'", function (err, result) {
+    pool.query("SELECT * FROM ARTICLE WHERE TITLE = $1", [req.params.articleName], function (err, result) {
        if (err) {
            res.status(500).send(err.toString());
        } else {
